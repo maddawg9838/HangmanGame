1854ab383f6437303e10f92145e4cb6943abceb7
maddawg9838 <125151938+maddawg9838@users.noreply.github.com>
2023-06-08 20:20:24 -0500

    Update main.cpp
    Finished the program for a single hangman game

--- main.cpp
+++ main.cpp
@@ -4,156 +4,106 @@
 
 using namespace std;
 
-void play ();
-void game ();
-void display ();
+void game();
+void message();
 
-//Function Main
+// Function Main
 int main()
 {
-play ();
-word();
-game ();
-display();
-return(0);
+    cout << "Let’s Play a Game!" << endl;
+    cout << "We are gonna play hangman" << endl;
+    cout << "First we will have the first person choose a word and then the second person will guess" << endl
+         << endl;
+    game();
+    return (0);
 }
 
-
-//Function Play
-void play ()
+void game()
 {
-char decision;
+    int wordSize, count = 0, strikes = 0;
+    char guess;
+    bool win = false;
 
-cout << "Let’s Play a Game!" << endl;
-cout << "Do you wanna play? (Y for yes or N for no)" << endl;
-cin >> decision;
+    cout << "How long is the word you want your partner to guess?" << endl;
+    cin >> wordSize;
 
+    char theWord[wordSize];
+    bool match[wordSize];
 
-    while(decision != 'Y' && decision != 'y' && decision != 'N' && decision != 'n' )
+    for (int i = 0; i < wordSize; i++)
     {
-        cout << "Can you even spell? Try again" << endl;
-        cin >> decision;
+        theWord[i] = 'a';
+        match[wordSize] = false;
+    }
+
+    cout << "What is the word you will make your opponent guess, letter by letter?" << endl;
+    for (int i = 0; i < wordSize; i++)
+    {
+        cin >> theWord[i];
+    }
+
+    for (int i = 0; i < wordSize; i++)
+    {
+        cout << "__ ";
+    }
+
+    do
+    {
+        bool good = false;
         
-        if(decision == 'Y' || decision == 'y')
+        cout << "Please guess a letter. Remember 5 Strikes You are Out!!" << endl;
+        cin >> guess;
+
+        for (int i = 0; i < wordSize; i++)
         {
-            cout << "Yay!" << endl;
-            cout << "We are gonna play hangman" << endl;
-            cout << "Try and guess the statement" << endl << endl;
-            game();
+            if (theWord[i] == guess)
+            {
+                match[i] = true;
+                count++;
+                good = true;
+            }
         }
-        else if (decision == 'N' || decision == 'n')
+
+        if (good == false)
         {
-            cout << "Then why did you open this program?" << endl;
+            strikes++;
+            cout << "You now have " << strikes << " amount of strikes" << endl;
+        }
+
+        if (count == wordSize)
+        {
+            message();
             return;
         }
-    }
+
+        if (strikes == 5)
+        {
+            cout << "You have run out of attempts" << endl;
+            return;
+        }
+
+        for (int i = 0; i < wordSize; i++)
+        {
+            if (match[i] == true)
+            {
+                cout << theWord[i] << " ";
+            }
+            else
+            {
+                cout << "___ ";
+                ;
+            }
+        }
+
+    } while (win == false);
+    return;
 }
 
-void word()
+void message()
 {
-    string theWord;
-
-    cout << "What is the word you will make your opponent guess?" << endl;
-    cin >> theWord;
-
+    cout << "Congrats on guessing the correct word!";
+    return;
 }
 
-//Function Game
-void game ()
-{
-    char letter, letter1 = '_', letter2 = '_', letter3 = '_', letter4 = '_', letter5 = '_', letter6 = '_', letter7 = '_', letter8 = '_';
-    string statement;
-    int letters = 0;
-    bool win = false;
-    
-    cout << letter1 << " " << letter2 << letter3 << letter4 << letter5 << " " << letter6 << letter7 << letter8 << "!" << endl;
-    cout << "Well…give it a go and guess a letter (use all capital letters or I will judge you for not correctly reading this message" << endl;
-    cin >> letter;
-    
-    while (win == false)
-    {
-        switch (letter)
-        {
-            case 'I': {
-                letter1 = 'I';
-                cout << "Goodjob!" << endl;
-                letters++;
-                break;
-            }
-            case 'L':
-            {
-                letter2 = 'L';
-                cout << "Goodjob!" << endl;
-                letters++;
-                break;
-            }
-              
-            case 'O':
-            {
-                letter3 = 'O';
-                letter7 = 'O';
-                cout << "Double Goodjob!" << endl;
-                letters+= 2;
-                break;
-            }
-             
-            case 'V':
-            {
-                letter4 = 'V';
-                cout << "Goodjob!" << endl;
-                letters++;
-                break;
-            }
-           
-            case 'E':
-            {
-                letter5 = 'E';
-                cout << "Goodjob!" << endl;
-                letters++;
-                break;
-            }
-          
-            case 'Y':
-            {
-                letter6 = 'Y';
-                cout << "Goodjob!" << endl;
-                letters++;
-                break;
-            }
-           
-            case 'U':
-            {
-                letter8 = 'U';
-                cout << "Goodjob!" << endl;
-                letters++;
-                break;
-            }
-              
-            default:
-            {
-                cout << "Guess the right letter or read the instructions correctly" << endl;
-                cout << "Go ahead. Guess again. This time make it a good one" << endl;
-                cin >> letter;
-            }
-                
-        }
-        
-        if(letters == 8)
-        {
-            win = true;
-            cout << "You Guessed It!" << endl;
-            cout << letter1 << " " << letter2 << letter3 << letter4 << letter5 << " " << letter6 << letter7 << letter8 << "!" << endl;
-            
-            cout << "Wait, really? Thanks!" << endl << endl;
-            cout << "Just Kidding :)" << endl;
-        }
-        else
-        {
-            cout << letter1 << " " << letter2 << letter3 << letter4 << letter5 << " " << letter6 << letter7 << letter8 << "!" << endl;
-            cout << "Keep Guessing. You Got This!" << endl;
-            cin >> letter;
-        }
-    }
-}
 
 
